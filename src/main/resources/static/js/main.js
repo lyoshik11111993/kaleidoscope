66ea4d7c881230b748c0c3948073c5af63627b8e
lyoshik <lyoshikrul@gmail.com>
2017-10-27 20:33:28 +0300

    added auto animation without turning off

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,5 +1,3 @@
-
-
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
@@ -12,11 +10,24 @@ function getRandomColor() {
 function setColor() {
     var table = document.getElementById("table");
     var rows = table.getElementsByTagName("tr");
-    var cols = null;
-    for(var j=0; j<rows.length; j++) {
+    var cols;
+    for (var j = 0; j < rows.length; j++) {
         cols = rows[j].getElementsByTagName("td");
-        for( var i=0; i<cols.length; i++) {
+        for (var i = 0; i < cols.length; i++) {
             cols[i].bgColor = getRandomColor();
         }
     }
+}
+
+function autoAnimation(fnc, delay) {
+    var isOver = false;
+    var interval = setInterval(fnc, delay);
+    fnc();
+    var block = document.getElementById("auto_animation");
+    var p = document.createElement("p");
+    p.innerHTML = "   Tired of watching? -> ";
+    var btn = document.createElement("button");
+    btn.innerHTML = "Stop Animation";
+    block.replaceChild(p, document.getElementById("auto_animation_p"));
+    block.replaceChild(btn, document.getElementById("auto_animation_btn"));
 }
